e2107804a4941ae41f952cde0b6a34fb0df5fe10
John Casey <John Casey>
2016-07-10 21:15:53 -0400

    Add abstract, intro and some placeholders for the report later

---
+++ Report/test.py
@@ -0,0 +1,6 @@
+import sys
+
+print('Program args: ', sys.argv)
+
+for i in range(len(sys.argv)):
+	print(sys.argv[i])
---
+++ Report/test_code.c
@@ -0,0 +1,13 @@
+//Test code for listing package
+#include <stdio.h>
+
+int main(){
+   unsigned int counter = 1;
+
+   while(counter <= 10{
+      printf("%u ", counter);
+      ++counter;
+   }
+
+   puts("");
+}
